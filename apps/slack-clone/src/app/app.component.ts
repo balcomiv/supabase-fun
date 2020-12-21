a09ea6dab4c8319f8ae03f3464edc73db3449a7f
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2020-12-21 05:58:03 -0500

    initial commit

---
+++ apps/slack-clone/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'supabase-fun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'slack-clone';
+}
